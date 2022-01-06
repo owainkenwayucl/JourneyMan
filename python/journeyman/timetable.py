941de055baebef058d6448d15bc965c363ede24f
Owain Kenway <o.kenway@ucl.ac.uk>
2022-01-06 11:06:50 +0000

    Start timetable.

---
+++ python/journeyman/timetable.py
@@ -0,0 +1,10 @@
+class timetable:
+
+    def __init__(self, start, stop, frequency, bunch, delay, cancellation):
+        self.start = start
+        self.stop = stop
+        self.frequency = frequency
+        self.bunch = bunch
+        self.delay = delay
+        self.cancellation = cancellation
+
